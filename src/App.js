cf61454c4137f1256cc88f5f0e6c2e6ac4aa0664
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-11-07 13:36:54 +0530

    demo model added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React,{ useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 
 function App() {
   const [showParagraph,setShowParagraph] =  useState(false);
 
   console.log('App initialized');
-  
+
   const toggleParagraphHandler =() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
   };
   return (
     <div className="app">
       <h1>Hi there!</h1>
-     {showParagraph && <p>This is new!</p>}
+      <DemoOutput shoe={showParagraph}/>
+     {/* {showParagraph && <p>This is new!</p>} */}
      <button onClick={ toggleParagraphHandler }>Toggle Paragraph!</button>
     </div>
   );
---
+++ src/components/UI/Button/Demo/DemoOutput.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const DemoOutput = (prpos) => {
+    return <p>{prpos.show ? "this is new!" : "  "}</p>;
+};
+
+export default DemoOutput;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
