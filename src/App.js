db7b2b2d3b87e90f0d0f494916c3cc5f6ac55e1e
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-11-15 13:11:44 +0530

    toggle handler edited

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 
 function App() {
   const [showParagraph,setShowParagraph] =  useState(false);
-
+const [AllowToggle, setAllowToggle] = useState(false);
   console.log('App initialized');
 
   const toggleParagraphHandler =useCallback(() => {
-    if(allow)
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    if (AllowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
   }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph}/>
-      <Button onClick={allowToggleHandeler}>Allow Toggling</Button>
-     {/* {showParagraph && <p>This is new!</p>} */}
-     <Button onClick={ toggleParagraphHandler }>Toggle Paragraph!</Button>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={AllowToggleHandeler}>Allow Toggling</Button>
+      {/* {showParagraph && <p>This is new!</p>} */}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
